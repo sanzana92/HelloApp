a128fef8a06302d7d37404f30cfb43e6ffe84140
Sanzana Rreddy <sa5648@srmist.edu.in>
2026-03-25 13:05:54 +0530

    Coded UC7: Display Hello using String.join()

--- Desktop/JAVAAPP/HelloApp.java
+++ Desktop/JAVAAPP/HelloApp.java
@@ -1,18 +1,13 @@
 public class HelloApp {
-
     public static void main(String[] args) {
 
-        if (args.length > 0) {
-
-            String names = String.join(", ", args);
-            System.out.println("Hello, " + names + "!");
-
-        } else {
-
+        if (args.length == 0) {
             System.out.println("Hello, World!");
+        } 
+        else {
+            String names = String.join(", ", args);
 
+            System.out.println("Hello, " + names + "!");
         }
-
     }
-
 }
